3e93d241b951420007d01dc5172c5e258fb23261
Matias <50750958+mattii95@users.noreply.github.com>
2020-12-26 18:23:14 -0300

    CONEXION BT

--- projecto_glucometro/lib/src/pages/home_page.dart
+++ projecto_glucometro/lib/src/pages/home_page.dart
@@ -1,15 +1,126 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+import 'package:projecto_glucometro/src/BluetoothDeviceListEntry.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
+  BluetoothState _bluetoothState = BluetoothState.UNKNOWN;
+
+  List<BluetoothDevice> devices = List<BluetoothDevice>();
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    _getBTState();
+    _stateChangeListener();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state.index == 0) {
+      if (_bluetoothState.isEnabled) {
+        _listBondedDevices();
+      }
+    }
+  }
+
+  _getBTState() {
+    FlutterBluetoothSerial.instance.state.then((state) {
+      _bluetoothState = state;
+      if (_bluetoothState.isEnabled) {
+        _listBondedDevices();
+      }
+      setState(() {});
+    });
+  }
+
+  _stateChangeListener() {
+    FlutterBluetoothSerial.instance
+        .onStateChanged()
+        .listen((BluetoothState state) {
+      _bluetoothState = state;
+      if (_bluetoothState.isEnabled) {
+        _listBondedDevices();
+      } else {
+        devices.clear();
+      }
+      print('State is Enable: ${state.isEnabled}');
+      setState(() {});
+    });
+  }
+
+  _listBondedDevices() {
+    FlutterBluetoothSerial.instance
+        .getBondedDevices()
+        .then((List<BluetoothDevice> bondedDevice) {
+      devices = bondedDevice;
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        centerTitle: true,
-        title: Text('Glucometro'),
+        title: Text('Dispositivos'),
       ),
-      body: Center(
-        child: Text('hola mundo'),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            SwitchListTile(
+              title: Text('Enabled Bluetooth'),
+              value: _bluetoothState.isEnabled,
+              onChanged: (bool value) {
+                future() async {
+                  if (value) {
+                    await FlutterBluetoothSerial.instance.requestEnable();
+                  } else {
+                    await FlutterBluetoothSerial.instance.requestDisable();
+                  }
+                  future().then((_) {
+                    setState(() {});
+                  });
+                }
+              },
+            ),
+            ListTile(
+              title: Text('Bluetooth Status'),
+              subtitle: Text(_bluetoothState.toString()),
+              trailing: RaisedButton(
+                child: Text('Settings'),
+                onPressed: () {
+                  FlutterBluetoothSerial.instance.openSettings();
+                },
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                children: devices
+                    .map((_device) => BluetoothDeviceListEntry(
+                          device: _device,
+                          enabled: true,
+                          onTap: () {
+                            print('Estado de conexion:');
+                            print('${_device.isConnected}');
+                          },
+                        ))
+                    .toList(),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
